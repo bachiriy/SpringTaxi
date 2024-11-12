21e9df2903eda0880c2260644f78a4a7b3464b7b
salah <akilsalaheddine203@gmail.com>
2024-11-12 09:41:19 +0100

    implement a driver entity

--- app/src/main/java/com/springtaxi/app/entity/Driver.java
+++ app/src/main/java/com/springtaxi/app/entity/Driver.java
@@ -1,4 +1,30 @@
 package com.springtaxi.app.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "the first name must not be empty")
+    private String firstName;
+    @NotBlank(message = "the last name must not be empty")
+    private String lastName;
+
+    private LocalDate disponibiliteDebut;
+    private LocalDate disponibiliteFin;
+
+    @Enumerated(EnumType.STRING)
+    private DriverStatut statut;
+
+
 }
