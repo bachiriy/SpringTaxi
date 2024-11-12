f52ab66f27ab2e859be4e3d3846118090c97d28c
salah <akilsalaheddine203@gmail.com>
2024-11-12 16:21:13 +0100

    add mapper

--- app/src/main/java/com/springtaxi/app/mapper/DriverMapper.java
+++ app/src/main/java/com/springtaxi/app/mapper/DriverMapper.java
@@ -1,8 +1,20 @@
 package com.springtaxi.app.mapper;
 
+import com.springtaxi.app.dto.DriverDto;
+import com.springtaxi.app.entity.Driver;
 import org.mapstruct.Mapper;
 
 @Mapper
 public class DriverMapper {
+    public DriverDto toDto (Driver driver) {
+        return DriverDto.builder()
+                .id(driver.getId())
+                .firstName(driver.getFirstName())
+                .lastName(driver.getLastName())
+                .disponibiliteDebut(driver.getDisponibiliteDebut())
+                .disponibiliteFin(driver.getDisponibiliteFin())
+                .statut(driver.getStatut())
+                .build();
+    }
 
 }
