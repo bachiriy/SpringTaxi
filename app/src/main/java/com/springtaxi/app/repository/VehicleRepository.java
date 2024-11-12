6c1231416c8988f8f9fbf394c5694493a07395c7
Walid Saifi <saifiwalid21@gmail.com>
2024-11-12 16:19:49 +0100

    ajouter test

--- app/src/main/java/com/springtaxi/app/repository/VehicleRepository.java
+++ app/src/main/java/com/springtaxi/app/repository/VehicleRepository.java
@@ -1,8 +1,20 @@
 package com.springtaxi.app.repository;
 
 import com.springtaxi.app.entity.Vehicle;
+import com.springtaxi.app.entity.VehicleStatus;
+import com.springtaxi.app.entity.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VehicleRepository  extends JpaRepository<Vehicle, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    List<Vehicle> findByStatus(VehicleStatus status);
+
+    Optional<Vehicle> findByLicensePlate(String licensePlate);
+
+    List<Vehicle> findByType(VehicleType type);
+
+    Optional<Vehicle> findByModel(String model);
 }
