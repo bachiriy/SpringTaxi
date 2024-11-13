42e0afb2fd0be4cae014f4c4d0c75a53b2582849
salah <akilsalaheddine203@gmail.com>
2024-11-13 10:59:30 +0100

    implement interface driver mapper

--- app/src/main/java/com/springtaxi/app/entity/Driver.java
+++ app/src/main/java/com/springtaxi/app/entity/Driver.java
@@ -5,9 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -27,8 +29,12 @@ public class Driver {
 
     @NotBlank(message = "the last name must not be empty")
     private String lastName;
-
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    @NotNull
     private LocalDate disponibiliteDebut;
+
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    @NotNull
     private LocalDate disponibiliteFin;
 
     @Enumerated(EnumType.STRING)
